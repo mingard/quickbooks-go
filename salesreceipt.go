71bd2a87da73918c29215854bbabfc6d70376b52
mingard <info@arthurmingard.com>
2021-05-19 12:03:17 +0000

    chore: add missing values

---
+++ salesreceipt.go
@@ -0,0 +1,149 @@
+package quickbooks
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/url"
+)
+
+// SalesReceipt represents a QuickBooks SalesReceipt object.
+type SalesReceipt struct {
+	ID                  string        `json:"Id,omitempty"`
+	DocNumber           string        `json:",omitempty"`
+	PaymentRefNum       string        `json:",omitempty"`
+	TotalAmt            json.Number   `json:",omitempty"`
+	TxnDate             Date          `json:",omitempty"`
+	PrintStatus         string        `json:",omitempty"`
+	EmailStatus         string        `json:",omitempty"`
+	PaymentMethodRef    ReferenceType `json:",omitempty"`
+	DepositToAccountRef ReferenceType `json:",omitempty"`
+	// DepartmentRef
+	PrivateNote          string  `json:",omitempty"`
+	CustomerMemo         MemoRef `json:",omitempty"`
+	GlobalTaxCalculation string  `json:",omitempty"`
+	Line                 []Line  `json:",omitempty"`
+}
+
+// CreateInvoice creates the given Invoice on the QuickBooks server, returning
+// the resulting Invoice object.
+func (c *Client) CreateSalesReceipt(srp *SalesReceipt) (*SalesReceipt, error) {
+	var u, err = url.Parse(string(c.Endpoint))
+	if err != nil {
+		return nil, err
+	}
+	u.Path = "/v3/company/" + c.RealmID + "/salesreceipt"
+	var v = url.Values{}
+	v.Add("minorversion", minorVersion)
+	u.RawQuery = v.Encode()
+	var j []byte
+	j, err = json.Marshal(srp)
+	if err != nil {
+		return nil, err
+	}
+	var req *http.Request
+	req, err = http.NewRequest("POST", u.String(), bytes.NewBuffer(j))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Accept", "application/json")
+	var res *http.Response
+	res, err = c.Client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, parseFailure(res)
+	}
+
+	var r struct {
+		SalesReceipt SalesReceipt
+		Time         Date
+	}
+	err = json.NewDecoder(res.Body).Decode(&r)
+	return &r.SalesReceipt, err
+}
+
+/*
+
+{
+  "SalesReceipt": {
+    "DocNumber": "1003",
+    "SyncToken": "0",
+    "domain": "QBO",
+    "Balance": 0,
+    "PaymentMethodRef": {
+      "name": "Check",
+      "value": "2"
+    },
+    "BillAddr": {
+      "Lat": "INVALID",
+      "Long": "INVALID",
+      "Id": "49",
+      "Line1": "Dylan Sollfrank"
+    },
+    "DepositToAccountRef": {
+      "name": "Checking",
+      "value": "35"
+    },
+    "TxnDate": "2014-09-14",
+    "TotalAmt": 337.5,
+    "CustomerRef": {
+      "name": "Dylan Sollfrank",
+      "value": "6"
+    },
+    "CustomerMemo": {
+      "value": "Thank you for your business and have a great day!"
+    },
+    "PrintStatus": "NotSet",
+    "PaymentRefNum": "10264",
+    "EmailStatus": "NotSet",
+    "sparse": false,
+    "Line": [
+      {
+        "Description": "Custom Design",
+        "DetailType": "SalesItemLineDetail",
+        "SalesItemLineDetail": {
+          "TaxCodeRef": {
+            "value": "NON"
+          },
+          "Qty": 4.5,
+          "UnitPrice": 75,
+          "ItemRef": {
+            "name": "Design",
+            "value": "4"
+          }
+        },
+        "LineNum": 1,
+        "Amount": 337.5,
+        "Id": "1"
+      },
+      {
+        "DetailType": "SubTotalLineDetail",
+        "Amount": 337.5,
+        "SubTotalLineDetail": {}
+      }
+    ],
+    "ApplyTaxAfterDiscount": false,
+    "CustomField": [
+      {
+        "DefinitionId": "1",
+        "Type": "StringType",
+        "Name": "Crew #"
+      }
+    ],
+    "Id": "11",
+    "TxnTaxDetail": {
+      "TotalTax": 0
+    },
+    "MetaData": {
+      "CreateTime": "2014-09-16T14:59:48-07:00",
+      "LastUpdatedTime": "2014-09-16T14:59:48-07:00"
+    }
+  },
+  "time": "2015-07-29T09:29:56.229-07:00"
+}
+*/
